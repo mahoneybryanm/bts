7031c964c91357649e1031f75d23d7d7a65e4980
Bryan Mahoney <mahoneybryanm@gmail.com>
2014-09-02 23:33:02 -0400

    second commit

---
+++ Dropbox/CE290I/dlxa.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int op;
+    int a;
+    int b;
+    int c;
+    
+}
---
+++ Dropbox/CE290I/dlxarch.c
@@ -0,0 +1,6 @@
+/* DLX Architecture Attempt
+By: Bryan Mahoney
+*/
+
+int pc;
+int ir;
